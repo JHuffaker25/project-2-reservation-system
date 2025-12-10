9ddf7668d8aa09cc54c0af4b2ca9842e579c95a8
Ocean Hillman <oceanhillman@gmail.com>
2025-12-10 16:01:08 -0500

    Add CORS configuration and permit OPTIONS requests and room-types access

--- backend/src/main/java/com/skillstorm/backend/Config/SecurityConfig.java
+++ backend/src/main/java/com/skillstorm/backend/Config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.skillstorm.backend.Config;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -13,20 +20,39 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     
-        
         http
+            .cors(Customizer.withDefaults())
             .authorizeHttpRequests(authorize -> {
                 authorize
-                .anyRequest().authenticated();
+                
                 //.requestMatchers("/users/hello").permitAll()
                 //.requestMatchers("/users/private-info").authenticated()
+
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Allow CORS preflight requests
+                .requestMatchers("/room-types/**").permitAll()
+                
+                .anyRequest().authenticated();
             })
 
             //Tells Spring security to use registered oauth2 login configuration
             .oauth2Login(Customizer.withDefaults()); 
-
+            
 
         return http.build();
-        
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(
+        @Value("${FRONTEND_URL}") String frontendUrl
+    ) {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(frontendUrl));
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
