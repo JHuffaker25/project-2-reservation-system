3174a0d29232ee6c3588dcd328842ec5c06aa3cd
Ocean Hillman <oceanhillman@gmail.com>
2025-12-10 16:02:29 -0500

    change invalid Longs to Strings

--- backend/src/main/java/com/skillstorm/backend/Repositories/ReservationRepository.java
+++ backend/src/main/java/com/skillstorm/backend/Repositories/ReservationRepository.java
@@ -3,6 +3,6 @@ package com.skillstorm.backend.Repositories;
 import com.skillstorm.backend.Models.Reservation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ReservationRepository extends MongoRepository<Reservation, Long> {
+public interface ReservationRepository extends MongoRepository<Reservation, String> {
     // Custom query methods if needed
 }
