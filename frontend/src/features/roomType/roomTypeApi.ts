abd6a5631f17160df22e22e3f979894cf5ad4344
Ocean Hillman <oceanhillman@gmail.com>
2025-12-22 17:31:41 -0500

    Add get room type by reservation id query, remove unused query

--- frontend/src/features/roomType/roomTypeApi.ts
+++ frontend/src/features/roomType/roomTypeApi.ts
@@ -2,39 +2,36 @@ import { baseApi } from "@/app/baseApi";
 import type { RoomType } from "@/features/roomType/roomTypeSlice";
 
 export const roomTypeApi = baseApi.injectEndpoints({
-    endpoints: (builder) => ({
-        getRoomTypes: builder.query<RoomType[], void>({
-            query: () => ({
-                url: '/room-types/all',
-                method: 'GET',
-            }),
-            transformResponse: (response: RoomType[]) => {
-                return response;
-            }
-        }),
-        getRoomTypeById: builder.query<RoomType, string>({
-            query: (id: string) => ({
-                url: `/room-types/${id}`,
-                method: 'GET',
-            }),
-            transformResponse: (response: RoomType) => {
-                return response;
-            }
-        }),
-        getRoomAvailability: builder.query<RoomType[], { id: string; checkIn: Date; checkOut: Date }>({
-            query: ({ id, checkIn, checkOut }) => ({
-                url: `/room-types/${id}/available?dates=${checkIn}&dates=${checkOut}`,
-                method: 'GET',
-            }),
-            transformResponse: (response: RoomType[]) => {
-                return response;
-            },
-            transformErrorResponse: (response: { status: number; data: { message: string } }) => {
-                return response.data.message;
-            }
-        }),
+  endpoints: (builder) => ({
+    getRoomTypes: builder.query<RoomType[], void>({
+      query: () => ({
+        url: '/room-types/all',
+        method: 'GET',
+      }),
+      transformResponse: (response: RoomType[]) => {
+        return response;
+      }
     }),
-    overrideExisting: false,
+    getRoomTypeById: builder.query<RoomType, string>({
+      query: (id: string) => ({
+        url: `/room-types/${id}`,
+        method: 'GET',
+      }),
+      transformResponse: (response: RoomType) => {
+        return response;
+      }
+    }),
+    getRoomTypeByReservationId: builder.query<RoomType, string>({
+      query: (id: string) => ({
+        url: `/room-types/by-reservation/${id}`,
+        method: 'GET',
+      }),
+      transformResponse: (response: RoomType) => {
+        return response;
+      }
+    }),
+  }),
+  overrideExisting: false,
 });
 
-export const { useGetRoomTypesQuery, useGetRoomTypeByIdQuery } = roomTypeApi;
+export const { useGetRoomTypesQuery, useGetRoomTypeByIdQuery, useGetRoomTypeByReservationIdQuery } = roomTypeApi;
