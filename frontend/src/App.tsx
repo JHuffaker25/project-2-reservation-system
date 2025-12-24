6844f740d3a803803cf59a0ec5fb6d6aa9b242e8
Ocean Hillman <oceanhillman@gmail.com>
2025-12-24 14:24:02 -0500

    Add theme sync hook to app and stripe wrapper to profile for payment method management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router'
 import { useCsrfToken } from './app/useCsrfToken'
+import { useSyncThemeWithUser } from './app/useSyncThemeWithUser'
 import Layout from './components/layout'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
@@ -8,13 +9,18 @@ import Reservations from './pages/Reservations'
 import ViewTransactions from './pages/ViewTransactions'
 import LandingPage from './pages/LandingPage'
 import Profile from './pages/Profile'
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import ManageReservations from './pages/ManageReservations'
 import UpdateRooms from './pages/UpdateRooms'
 import BookReservation from './pages/BookReservation'
 import { ProtectedLayout } from './components/protected-layout'
 
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY!);
+
 function App() {
     useCsrfToken()
+    useSyncThemeWithUser()
     return (
         <Routes>
             <Route element={<Layout />}>
@@ -24,10 +30,15 @@ function App() {
                 <Route path="/rooms" element={<Rooms />} />
             </Route>
 
+
             <Route element={<ProtectedLayout requires="CUSTOMER" />}>
-                <Route path="/reservations" element={<Reservations />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/rooms/:id/book" element={<BookReservation />} />
+                    <Route path="/reservations" element={<Reservations />} />
+                    <Route path="/profile" element={
+                        <Elements stripe={stripePromise}>
+                            <Profile />
+                        </Elements>
+                    } />
+                    <Route path="/rooms/:id/book" element={<BookReservation />} />
             </Route>
 
             <Route element={<ProtectedLayout requires="admin" />}>
