481ab622233ae3fbb0bbfc12b1d4a69b2db686f2
Ishan Sultan <ishansultan1@gmail.com>
2025-12-26 12:32:40 -0600

    backend deployment working

--- backend/src/main/java/com/skillstorm/backend/Config/MongoConfig.java
+++ backend/src/main/java/com/skillstorm/backend/Config/MongoConfig.java
@@ -1,23 +1,24 @@
 package com.skillstorm.backend.Config;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManagerFactory;
-import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 
 @Configuration
+@SuppressWarnings("UnnecessaryReturnStatement")
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Value("${spring.data.mongodb.uri:${spring.mongodb.uri}}")
@@ -33,43 +34,47 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
     @Bean
     public MongoClient mongoClient() {
         try {
-            // Load ALL AWS DocumentDB certificates from the bundle
-            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            // Create truststore following official AWS DocumentDB approach
+            String truststorePassword = "changeit";
+            File truststoreFile = createTrustStore(truststorePassword);
 
-            // Create a KeyStore to hold all certificates from the bundle
-            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            keyStore.load(null, null);
+            // Set system properties for SSL/TLS
+            System.setProperty("javax.net.ssl.trustStore", truststoreFile.getAbsolutePath());
+            System.setProperty("javax.net.ssl.trustStorePassword", truststorePassword);
 
-            // Load all certificates from the PEM bundle
-            try (InputStream certInputStream = new ClassPathResource("global-bundle.pem").getInputStream()) {
-                int certIndex = 0;
-                for (Certificate cert : cf.generateCertificates(certInputStream)) {
-                    keyStore.setCertificateEntry("documentdb-ca-" + certIndex++, cert);
-                }
-            }
-
-            // Create a TrustManager that trusts all certificates in the keystore
-            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            tmf.init(keyStore);
-
-            // Create an SSLContext that uses the TrustManager
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, tmf.getTrustManagers(), null);
-
-            // Build MongoDB client settings with SSL context
-            MongoClientSettings settings = MongoClientSettings.builder()
-                    .applyConnectionString(new ConnectionString(mongoUri))
-                    .applyToSslSettings(builder -> {
-                        builder.enabled(true);
-                        builder.invalidHostNameAllowed(true);
-                        builder.context(sslContext);
-                    })
-                    .build();
-
-            return MongoClients.create(settings);
+            // Create MongoDB client with connection string
+            return MongoClients.create(mongoUri);
 
         } catch (Exception e) {
             throw new RuntimeException("Failed to configure MongoDB SSL certificate", e);
         }
+    }
+
+    private File createTrustStore(String password) throws Exception {
+        // Load certificate factory
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+        // Create a KeyStore to hold all certificates
+        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        keyStore.load(null, null);
+
+        // Load all certificates from the PEM bundle
+        try (InputStream certInputStream = new ClassPathResource("global-bundle.pem").getInputStream()) {
+            int certIndex = 0;
+            for (Certificate cert : cf.generateCertificates(certInputStream)) {
+                String alias = "rds-ca-" + certIndex++;
+                keyStore.setCertificateEntry(alias, cert);
+            }
+        }
+
+        // Save keystore to temporary file
+        File truststoreFile = File.createTempFile("rds-truststore", ".jks");
+        truststoreFile.deleteOnExit();
+
+        try (FileOutputStream fos = new FileOutputStream(truststoreFile)) {
+            keyStore.store(fos, password.toCharArray());
+        }
+
+        return truststoreFile;
     }
 }
